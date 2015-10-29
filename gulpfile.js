25e88f0552a30a24d1047e495717068ad0885dc8
Jan Muehlemann <jan.muehlemann@gmail.com>
2015-10-29 09:11:16 +0100

    update publish

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,11 @@
 var gulp = require('gulp'),
+    babel = require('gulp-babel'),
+    prompt = require('gulp-prompt'),
+    git = require('gulp-git'),
+    bump = require('gulp-bump'),
+    filter = require('gulp-filter'),
+    tag_version = require('gulp-tag-version'),
+    shell = require('gulp-shell'),
     argv = require('yargs').argv,
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
@@ -7,15 +14,18 @@ var gulp = require('gulp'),
     buffer = require('vinyl-buffer'),
     browserify = require('browserify'),
     watchify = require('watchify'),
-    babel = require('babelify'),
+    babelify = require('babelify'),
     eslint = require('gulp-eslint'),
     Server = require('karma').Server;
 
-var entry = 'XHR.js',
+var pkg = require('./package.json');
+
+var entry = 'index.js',
+    standaloneName = 'i18nextXHRBackend',
     output = 'index.js';
 
 function compile(watch) {
-  var bundler = browserify('./lib/' + entry, { debug: argv.debug, standalone: 'i18nextXHRBackend' }).transform(babel);
+  var bundler = browserify('./src/' + entry, { debug: argv.debug, standalone: standaloneName }).transform(babelify);
   if (watch) {
     bundle = watchify(bundler);
   }
@@ -26,8 +36,8 @@ function compile(watch) {
       .pipe(source(output))
       .pipe(buffer())
       .pipe(gulpif(!argv.debug, uglify()))
-      .pipe(sourcemaps.init({ loadMaps: true }))
-      .pipe(sourcemaps.write('./'))
+      .pipe(gulpif(argv.debug, sourcemaps.init({ loadMaps: true })))
+      .pipe(gulpif(argv.debug, sourcemaps.write('./')))
       .pipe(gulp.dest('./bin'));
   }
 
@@ -71,11 +81,86 @@ gulp.task('tdd', function (done) {
   }, done).start();
 });
 
+gulp.task('babel', function () {
+  return gulp.src('./src/**/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('./lib'));
+});
+
+function inc(version) {
+  if (!version) return;
+
+  var type;
+
+  if (version.indexOf('.') < 0) {
+    if (version === 'major' || version === 'minor' || version === 'patch') {
+      type = version;
+    } else {
+      var tag = version;
+
+      var parts = pkg.version.split('-');
+      if (parts.length > 1) {
+        var tagVersion = 0;
+        var p = parts[1].split('.');
+        if (p[0] === tag) tagVersion = parseInt(p[1], 10) + 1;
+        version = parts[0] + '-' + tag  + '.' + tagVersion;
+      } else {
+        version = pkg.version + '-' + tag  + '.0';
+      }
+    }
+  }
+
+  function cb(obj) {
+    var stream = new require('stream').Transform({objectMode: true});
+    stream._transform = function(file, unused, callback) {
+      obj();
+      callback(null, file);
+    };
+    return stream;
+  }
+
+  // get all the files to bump version in
+  return gulp.src(['./package.json', './bower.json'])
+    // bump the version number in those files
+    .pipe(type ? bump({type: type}) : bump({version: version}))
+    // save it back to filesystem
+    .pipe(gulp.dest('./'))
+
+    // read only one file to get the version number
+    .pipe(filter('package.json'))
+    // **tag it in the repository**
+    .pipe(tag_version({prefix: ''}))
+
+    // push tag
+    .pipe(prompt.confirm({
+        message: 'Commit and Push tag ' + version + ' to github?',
+        default: false
+    }))
+    .pipe(git.commit('bumps package version'))
+    .pipe(cb(function() {
+      git.push('origin','master', {args: ' --tags'}, function (err) {
+        if (err) throw err;
+      });
+    }))
+
+    // npm publish
+    .pipe(prompt.confirm({
+        message: 'publish ' + version + ' to npm?',
+        default: false
+    }))
+    .pipe(shell([
+      'npm publish'
+    ]));
+}
+
 function watch() {
   return compile(true);
 };
 
-gulp.task('build', function() { return compile(); });
+gulp.task('concat', function() { return compile(); });
 gulp.task('watch', function() { return watch(); });
+gulp.task('bump', function() { return inc(argv.v); });
 
 gulp.task('default', ['watch']);
+gulp.task('build', ['concat', 'babel']);
+gulp.task('publish', ['bump']);
