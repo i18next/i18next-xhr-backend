f2d53da00c6e844b7706c798107c6953809958e4
Kevin Ross <kevin.ross@alienfast.com>
2019-08-02 09:47:38 -0500

    attempt (failed) to use Interpolator, ResourceStore, etc from i18next.services

--- test/backendConnector.load.spec.js
+++ test/backendConnector.load.spec.js
@@ -1,24 +1,31 @@
-import BackendConnector from 'i18next/dist/commonjs/BackendConnector.js';
+import i18next from 'i18next';
 import XHR from '../src/';
-import Interpolator from 'i18next/dist/commonjs/Interpolator';
-import ResourceStore from 'i18next/dist/commonjs/ResourceStore.js';
 
 describe('BackendConnector basic load', () => {
   let connector;
 
   before(() => {
-    connector = new BackendConnector(new XHR(), new ResourceStore(), {
-      interpolator: new Interpolator()
-    }, {
-      backend: { loadPath: 'http://localhost:9876/locales/{{lng}}/{{ns}}.json' }
-    });
+    connector = new i18next.services.backendConnector(
+      new XHR(),
+      i18next.services.resourceStore,
+      {
+        interpolator: i18next.services.interpolator,
+      },
+      {
+        backend: {
+          loadPath: 'http://localhost:9876/locales/{{lng}}/{{ns}}.json',
+        },
+      },
+    );
   });
 
   describe('#load', () => {
-    it('should load data', (done) => {
+    it('should load data', done => {
       connector.load(['en'], ['test'], function(err) {
         expect(err).to.be.not.ok;
-        expect(connector.store.getResourceBundle('en', 'test')).to.eql({key: 'passing'});
+        expect(connector.store.getResourceBundle('en', 'test')).to.eql({
+          key: 'passing',
+        });
         done();
       });
     });
--- test/xhr.spec.js
+++ test/xhr.spec.js
@@ -1,56 +1,62 @@
 import XHR from '../src/';
-import Interpolator from 'i18next/dist/commonjs/Interpolator';
+import i18next from 'i18next';
 import JSON5 from 'json5';
 
 describe('XHR backend', () => {
-
   describe('#read', () => {
     let backend;
 
     before(() => {
-      backend = new XHR({
-        interpolator: new Interpolator()
-      }, {
-        loadPath: 'http://localhost:9876/locales/{{lng}}/{{ns}}.json'
-      });
+      backend = new XHR(
+        {
+          interpolator: i18next.services.interpolator,
+        },
+        {
+          loadPath: 'http://localhost:9876/locales/{{lng}}/{{ns}}.json',
+        },
+      );
     });
 
-    it('should load data', (done) => {
+    it('should load data', done => {
       backend.read('en', 'test', function(err, data) {
         expect(err).to.be.not.ok;
-        expect(data).to.eql({key: 'passing'});
+        expect(data).to.eql({ key: 'passing' });
         done();
       });
     });
 
-    it('should throw error on not existing file', (done) => {
+    it('should throw error on not existing file', done => {
       backend.read('en', 'notexisting', function(err, data) {
         expect(err).to.equal('failed loading http://localhost:9876/locales/en/notexisting.json');
         done();
       });
     });
 
-    it('should throw error on non json file', (done) => {
+    it('should throw error on non json file', done => {
       backend.read('en', 'nonjson', function(err, data) {
-        expect(err).to.equal('failed parsing http://localhost:9876/locales/en/nonjson.json to json');
+        expect(err).to.equal(
+          'failed parsing http://localhost:9876/locales/en/nonjson.json to json',
+        );
         done();
       });
     });
 
-    it('should load json5 data', (done) => {
-      backend = new XHR({
-        interpolator: new Interpolator()
-      }, {
-        loadPath: 'http://localhost:9876/locales/{{lng}}/{{ns}}.json5',
-        parse: JSON5.parse
-      });
+    it('should load json5 data', done => {
+      backend = new XHR(
+        {
+          interpolator: i18next.services.interpolator,
+        },
+        {
+          loadPath: 'http://localhost:9876/locales/{{lng}}/{{ns}}.json5',
+          parse: JSON5.parse,
+        },
+      );
       backend.read('en', 'test', function(err, data) {
         expect(err).to.be.not.ok;
-        expect(data).to.eql({key: 'passing'});
+        expect(data).to.eql({ key: 'passing' });
         done();
       });
     });
-
   });
 
   describe('with loadPath function', () => {
@@ -60,26 +66,25 @@ describe('XHR backend', () => {
     });
 
     before(() => {
-      backend = new XHR({
-        interpolator: new Interpolator()
-      }, {
-        loadPath: loadPathSpy
-      });
+      backend = new XHR(
+        {
+          interpolator: i18next.services.interpolator,
+        },
+        {
+          loadPath: loadPathSpy,
+        },
+      );
     });
 
     describe('#read', () => {
-
-      it('should load data', (done) => {
+      it('should load data', done => {
         backend.read('en', 'test', function(err, data) {
           expect(err).to.be.not.ok;
           expect(loadPathSpy.calledWith(['en'], ['test'])).to.be.ok;
-          expect(data).to.eql({key: 'passing'});
+          expect(data).to.eql({ key: 'passing' });
           done();
         });
       });
-
     });
-
   });
-
 });
